6ba00ffab6f36f20b80591ee6ae208b19d229a8f
Victor <victorstafy11@gmail.com>
2022-08-27 00:48:20 -0300

    api rejeitando post faltando entradas esperadas

--- src/index.js
+++ src/index.js
@@ -12,24 +12,29 @@ let _user_avatar='';
 
 server.post('/sign-up',(req,res)=>{
     const user_profile=req.body;
-    console.log(user_profile);
+
+    if(!user_profile.avatar || !user_profile.username ){
+        return res.status(400).send("Todos os campos são obrigatórios!")
+    }
+
     _user_avatar=(user_profile.avatar).slice();
-    res.send('OK');
-    console.log('post_sign-up');
+    res.status(201).send('OK');
 });
 
 server.post('/tweets',(req,res)=>{
     const tweet=req.body;
-    console.log(_user_avatar)
+
+    if(!tweet.tweet || !tweet.username ){
+        return res.status(400).send("Todos os campos são obrigatórios!")
+    }
+
     tweet.avatar=_user_avatar;
     tweets_list.push(tweet);
-    res.send('OK');
-    console.log('post_tweets');
+    res.status(201).send('OK');
 });
 
 server.get('/tweets',(req,res)=>{
     res.send(tweets_list.slice(-10));
-    console.log('get_tweets');
 })
 
 server.listen(5000,function(){console.log('ouvindo')});
